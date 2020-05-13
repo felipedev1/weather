c9bf986521fe2553b976558dbd09b91efd9f1a7d
Felipe <felipe.pereira.dev@gmail.com>
2020-05-12 22:22:50 -0300

    Updates

---
+++ precache-manifest.81e2ca6babf962f4d2fd4d5463a52488.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca57d2668ece01daceef929134268d8",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "40b0acc5ce17d3020384",
+    "url": "/weather/static/css/main.0b8028c7.chunk.css"
+  },
+  {
+    "revision": "2ed22a343a97cde2cd0f",
+    "url": "/weather/static/js/2.1ba765d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.1ba765d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b0acc5ce17d3020384",
+    "url": "/weather/static/js/main.ddaecfef.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
